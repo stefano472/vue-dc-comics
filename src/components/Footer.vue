78e59e9857e72ae92b78a5364915daef60f1667d
stefano472 <stefanopiotti472@gmail.com>
2022-04-07 00:43:00 +0200

    adjustment

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,94 +1,96 @@
 <template>
   <footer>
-    <div class="hero-footer container">
-      <div class="footer-comics-nav">
-        <h2>DC COMICS</h2>
-        <ul>
-          <li>
-            <a href="#">Characters</a>
-          </li>
-          <li>
-            <a href="#">Comics</a>
-          </li>
-          <li>
-            <a href="#">Movies</a>
-          </li>
-          <li>
-            <a href="#">TV</a>
-          </li>
-          <li>
-            <a href="#">Games</a>
-          </li>
-          <li>
-            <a href="#">Videos</a>
-          </li>
-          <li>
-            <a href="#">News</a>
-          </li>
-        </ul>
-        <h2>SHOP</h2>
-        <ul>
-          <li>
-            <a href="#">Shop DC</a>
-          </li>
-          <li>
-            <a href="#">Shop DC Collectibles</a>
-          </li>
-        </ul>
-      </div>
+    <div class="hero-footer">
+      <div class="container">
+        <div class="footer-comics-nav">
+          <h2>DC COMICS</h2>
+          <ul>
+            <li>
+              <a href="#">Characters</a>
+            </li>
+            <li>
+              <a href="#">Comics</a>
+            </li>
+            <li>
+              <a href="#">Movies</a>
+            </li>
+            <li>
+              <a href="#">TV</a>
+            </li>
+            <li>
+              <a href="#">Games</a>
+            </li>
+            <li>
+              <a href="#">Videos</a>
+            </li>
+            <li>
+              <a href="#">News</a>
+            </li>
+          </ul>
+          <h2>SHOP</h2>
+          <ul>
+            <li>
+              <a href="#">Shop DC</a>
+            </li>
+            <li>
+              <a href="#">Shop DC Collectibles</a>
+            </li>
+          </ul>
+        </div>
 
-      <div class="footer-DC-nav">
-        <h2>DC</h2>
-        <ul>
-          <li>
-            <a href="#">Characters</a>
-          </li>
-          <li>
-            <a href="#">Comics</a>
-          </li>
-          <li>
-            <a href="#">Movies</a>
-          </li>
-          <li>
-            <a href="#">TV</a>
-          </li>
-          <li>
-            <a href="#">Games</a>
-          </li>
-          <li>
-            <a href="#">Videos</a>
-          </li>
-          <li>
-            <a href="#">News</a>
-          </li>
-        </ul>
-      </div>
+        <div class="footer-DC-nav">
+          <h2>DC</h2>
+          <ul>
+            <li>
+              <a href="#">Characters</a>
+            </li>
+            <li>
+              <a href="#">Comics</a>
+            </li>
+            <li>
+              <a href="#">Movies</a>
+            </li>
+            <li>
+              <a href="#">TV</a>
+            </li>
+            <li>
+              <a href="#">Games</a>
+            </li>
+            <li>
+              <a href="#">Videos</a>
+            </li>
+            <li>
+              <a href="#">News</a>
+            </li>
+          </ul>
+        </div>
 
-      <div class="footer-site-nav">
-        <h2>SITES</h2>
-        <ul>
-          <li>
-            <a href="#">Characters</a>
-          </li>
-          <li>
-            <a href="#">Comics</a>
-          </li>
-          <li>
-            <a href="#">Movies</a>
-          </li>
-          <li>
-            <a href="#">TV</a>
-          </li>
-          <li>
-            <a href="#">Games</a>
-          </li>
-          <li>
-            <a href="#">Videos</a>
-          </li>
-          <li>
-            <a href="#">News</a>
-          </li>
-        </ul>
+        <div class="footer-site-nav">
+          <h2>SITES</h2>
+          <ul>
+            <li>
+              <a href="#">Characters</a>
+            </li>
+            <li>
+              <a href="#">Comics</a>
+            </li>
+            <li>
+              <a href="#">Movies</a>
+            </li>
+            <li>
+              <a href="#">TV</a>
+            </li>
+            <li>
+              <a href="#">Games</a>
+            </li>
+            <li>
+              <a href="#">Videos</a>
+            </li>
+            <li>
+              <a href="#">News</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
 
@@ -140,40 +142,47 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-footer {
+
+.hero-footer {
   background-image: url("../assets/footer-bg.jpg");
   background-size: cover;
-  background-repeat: no-repeat;
-}
-.hero-footer {
-  padding: 1rem 0;
-  display: flex;
-  gap: 2rem;
-  background-image: url("../assets/dc-logo-bg.png");
-  background-repeat: no-repeat;
-  background-position: center right;
-  background-size: 450px;
-  &>div{
-    padding-bottom: 1.5rem;
-  }
-  h2 {
-    color: white;
-    font-size: 1rem;
-    font-weight: 500;
-    padding: 0.8rem 0;
-  }
-  ul {
-    list-style: none;
-    a {
-      color: #999;
-      text-decoration: none;
-      font-size: 0.8rem;
-      &:hover {
-        color: #008dff;
+
+  .container {
+    padding: 1rem 0;
+    display: flex;
+    gap: 2rem;
+    background-image: url("../assets/dc-logo-bg.png");
+    background-repeat: no-repeat;
+    background-position: center right;
+    background-size: 450px;
+
+    &>div{
+      padding-bottom: 1.5rem;
+    }
+
+    h2 {
+      color: white;
+      font-size: 1rem;
+      font-weight: 500;
+      padding: 0.8rem 0;
+    }
+
+    ul {
+      list-style: none;
+
+      a {
+        color: #999;
+        text-decoration: none;
+        font-size: 0.8rem;
+        
+        &:hover {
+          color: #008dff;
+        }
       }
     }
   }
 }
+
 .bottom-footer {
   background: #303030;
 
@@ -183,41 +192,41 @@ footer {
     align-items: center;
     padding: 1.5rem 0;
   }
-}
 
-button {
-  padding: 0.6rem;
-  background: none;
-  color: white;
-  font-weight: 600;
-  cursor: pointer;
-  border: 2px solid #008dff;
-  border-radius: 2px;
-  &:hover {
-    background: #008dff;
+  button {
+    padding: 0.6rem;
+    background: none;
+    color: white;
+    font-weight: 600;
+    cursor: pointer;
+    border: 2px solid #008dff;
+    border-radius: 2px;
+    &:hover {
+      background: #008dff;
+    }
   }
-}
 
-.contacts {
-  display: flex;
-  align-items: center;
-  gap: 1rem;
-  h2 {
-    color: #008dff;
-    font-size: 1rem;
-  }
-  .menu {
-    list-style: none;
+  .contacts {
     display: flex;
     align-items: center;
     gap: 1rem;
-    a {
-      display: flex;
+    h2 {
+      color: #008dff;
+      font-size: 1rem;
     }
-    img {
-      height: 1.8rem;
-      &:hover {
-        opacity: 0.4;
+    .menu {
+      list-style: none;
+      display: flex;
+      align-items: center;
+      gap: 1rem;
+      a {
+        display: flex;
+      }
+      img {
+        height: 1.8rem;
+        &:hover {
+          opacity: 0.4;
+        }
       }
     }
   }
