992610a3e298b773ae57debf1b3c932e0b0ff97f
stefano472 <stefanopiotti472@gmail.com>
2022-04-06 17:34:04 +0200

    ex done

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -94,7 +94,39 @@
 
     <div class="bottom-footer">
       <div class="container">
-        <h2>sign up now</h2>
+        <div class="action">
+          <button type="button">SIGN-UP NOW!</button>
+        </div>
+        <div class="contacts">
+          <h2>FOLLOW US</h2>
+          <ul class="menu">
+            <li>
+              <a href="#">
+                <img src="../assets/footer-facebook.png" alt="facebook">
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="../assets/footer-twitter.png" alt="twitter">
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="../assets/footer-youtube.png" alt="youtube">
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="../assets/footer-pinterest.png" alt="pinterest">
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="../assets/footer-periscope.png" alt="periscope">
+              </a>
+            </li>
+          </ul>       
+        </div>
       </div>
     </div>
   </footer>
@@ -141,6 +173,44 @@ footer {
 }
 .bottom-footer {
   background: #303030;
+
+  .container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1.5rem 0;
+  }
+}
+
+button {
+  padding: 0.6rem;
+  background: none;
   color: white;
+  font-weight: 600;
+  cursor: pointer;
+  border: none;
+  outline: 2px solid #008dff;
+}
+
+.contacts {
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  h2 {
+    color: #008dff;
+    font-size: 1rem;
+  }
+  .menu {
+    list-style: none;
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    a {
+      display: flex;
+    }
+    img {
+      height: 1.8rem;
+    }
+  }
 }
 </style>
